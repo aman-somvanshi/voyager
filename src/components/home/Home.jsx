64045795ebb9bd22cd87189fff10ffab1f7abde4
Shivani Rajput <shivanirajpoot998@gmail.com>
2025-04-28 17:26:09 +0530

    Footer fixed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,23 +6,30 @@ import Transport from './Transport';
 import SearchBar from './Searchbar';
 import More from './More';
 import Cards1 from './homeCards/Cards';
+import Footer from "./../footer/FooterMain";
+import {BrowserRouter} from 'react-router-dom';
 function Home() {
 
   return (
-    <div className="app-container" style={{fontFamily : "Roboto"}}>
-      <div className="transport-wrapper">
-        <Transport/>
+    <BrowserRouter>
+      <div className="app-container" style={{fontFamily : "Roboto"}}>
+        <div className="transport-wrapper">
+          <Transport/>
+        </div>
+        <div className="search-wrapper">
+          <SearchBar/>
+        </div>
+        <div className="more-wrapper">
+          <More/>
+        </div>
+        <div>
+          <Cards1 className="card-wrapper"/>
+        </div>
+        <div style = {{marginTop:"60px"}}>
+          <Footer ></Footer>
+        </div>
       </div>
-      <div className="search-wrapper">
-        <SearchBar/>
-      </div>
-      <div className="more-wrapper">
-        <More/>
-      </div>
-      <div>
-        <Cards1 className="card-wrapper"/>
-      </div>
-    </div>
+    </BrowserRouter>
   
   );
 }
