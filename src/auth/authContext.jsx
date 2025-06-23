023aeee25b83b415eed6c6db29ca842f750dca74
aman <amansomvanshi2002@gmail.com>
2025-06-23 16:12:05 +0530

    Made some changes to avoid module errors

--- src/auth/authContext.jsx
+++ src/auth/authContext.jsx
@@ -14,7 +14,7 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await fetch('http://localhost:3000/users'); // for users.json
+        const response = await fetch('http://localhost:3000/users');
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
